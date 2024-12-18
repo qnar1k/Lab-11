acf5fd7938d748c4beed5f1b7feb0a5a68619ed3
Qnar <150032504+qnar1k@users.noreply.github.com>
2024-12-18 15:20:37 +0400

    Update MainClient.cpp

--- MainClient.cpp
+++ MainClient.cpp
@@ -1,75 +1,96 @@
 #include "UDPSocket.h"
-#include <stdio.h>
-#include <winsock2.h>
 #include <ws2tcpip.h>
-#ifdef CLIENT
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+using namespace std;
+
+
+#define SERVER "127.0.0.1"
+#define PORT 8888
+#define BUFLEN 512
+
+
+#define THREAD_LOCK std::mutex
+#define THREADFUNCVAR void*
+#define THREADFUNCARGS void*
+#define THREADVAR std::thread
+
+
+THREAD_LOCK recv_lock;
+bool quitnow = false;
+
+
+void LockThread(THREAD_LOCK& lock) {
+    lock.lock();
+}
+
+void UnlockThread(THREAD_LOCK& lock) {
+    lock.unlock(); 
+}
+
+void InitThreadLock(THREAD_LOCK& lock) {
+
+}
+
+
+void ReceiveMessagesThread(UDPSocket* client_sock, sockaddr_in* si_other) {
+    int slen = sizeof(sockaddr_in);
+    char buf[BUFLEN] = { 0 };
+
+    while (!quitnow) {
+        memset(buf, 0, BUFLEN); 
+
+        LockThread(recv_lock); 
+        int bytesReceived = client_sock->RecvDatagram(buf, BUFLEN, (struct sockaddr*)si_other, &slen);
+        UnlockThread(recv_lock); 
+
+        if (bytesReceived > 0) {
+            cout << "\n[Server]: " << buf << endl;
+        }
+        else if (bytesReceived == SOCKET_ERROR) {
+            cerr << "Error receiving message: " << WSAGetLastError() << endl;
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));  
+    }
+}
+
+
 int main(int argc, char* argv[]) {
     struct sockaddr_in si_other;
     int slen = sizeof(si_other);
-    char buf[BUFLEN];
-    char message[BUFLEN];
+    char message[BUFLEN] = { 0 };
 
-    unsigned short srv_port = 0;
-
-    // Create socket
     UDPSocket client_sock;
+    InitThreadLock(recv_lock); 
 
-    // Setup address structure
+
     memset((char*)&si_other, 0, sizeof(si_other));
     si_other.sin_family = AF_INET;
+    si_other.sin_port = htons((argc > 1) ? atoi(argv[1]) : PORT);
+    inet_pton(AF_INET, (argc > 2) ? argv[2] : SERVER, &si_other.sin_addr);
 
-    // Convert IP address to a wide string (if necessary)
-    wchar_t wide_ip[256];
-    size_t outSize;
-    mbstowcs_s(&outSize, wide_ip, sizeof(wide_ip) / sizeof(wide_ip[0]), SERVER, strlen(SERVER) + 1);
+    cout << "Client started. Connected to server " << SERVER << " on port " << PORT << endl;
 
-    // Check if we are passing command-line arguments for server address and port
-    if (1 == argc) {
-        si_other.sin_port = htons(PORT);
-        if (InetPtonW(AF_INET, wide_ip, &si_other.sin_addr) <= 0) {  // Use InetPtonW
-            printf("Invalid address/Address not supported\n");
-            return 1;
-        }
-        printf("Server - addr=%s , port=%d\n", SERVER, PORT);
-    }
-    else if (2 == argc) {
-        si_other.sin_port = htons(atoi(argv[1]));
-        mbstowcs_s(&outSize, wide_ip, sizeof(wide_ip) / sizeof(wide_ip[0]), SERVER, strlen(SERVER) + 1);
-        if (InetPtonW(AF_INET, wide_ip, &si_other.sin_addr) <= 0) {  // Use InetPtonW
-            printf("Invalid address/Address not supported\n");
-            return 1;
-        }
-        printf("Server - addr=%s , port=%d\n", SERVER, atoi(argv[1]));
-    }
-    else {
-        si_other.sin_port = htons(atoi(argv[2]));
-        mbstowcs_s(&outSize, wide_ip, sizeof(wide_ip) / sizeof(wide_ip[0]), argv[1], strlen(argv[1]) + 1);
-        if (InetPtonW(AF_INET, wide_ip, &si_other.sin_addr) <= 0) {  // Use InetPtonW
-            printf("Invalid address/Address not supported\n");
-            return 1;
-        }
-        printf("Server - addr=%s , port=%d\n", argv[1], atoi(argv[2]));
-    }
 
-    // Start communication loop
-    while (1) {
-        printf("\nEnter message: ");
-        gets_s(message, BUFLEN);
+    THREADVAR recvThread(ReceiveMessagesThread, &client_sock, &si_other);
 
-        // Send the message to server
-        printf("Sending message: %s\n", message);  // Debug: print the message being sent
+
+    while (!quitnow) {
+        cout << "Enter message: ";
+        LockThread(recv_lock); 
+        cin.getline(message, BUFLEN);
         client_sock.SendDatagram(message, (int)strlen(message), (struct sockaddr*)&si_other, slen);
+        UnlockThread(recv_lock);  
 
-        // Clear the buffer before receiving a response
-        memset(buf, '\0', BUFLEN);
-
-        // Receive the server's response (blocking call)
-        client_sock.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other, &slen);
-
-        // Print the server's response
-        printf("Server Response: %s\n", buf);
+        if (string(message) == "exit") {
+            quitnow = true;
+        }
     }
+
+  
+    recvThread.join();
 
     return 0;
 }
-#endif
